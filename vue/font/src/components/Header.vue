f71516be9116a3881ffe3c63cd64368c7001a538
junior <trader.junior492047@gmail.com>
2019-12-27 03:23:46 +0700

    Delete nav-item on Header Vue

--- vue/font/src/components/Header.vue
+++ vue/font/src/components/Header.vue
@@ -3,15 +3,6 @@
   <b-navbar toggleable="lg" type="dark" variant="info">
     <b-container>
     <b-navbar-brand href="./first.vue">WASTE ENERGY</b-navbar-brand>
-
-    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-    <b-collapse id="nav-collapse" is-nav >
-      <b-navbar-nav class="ml-auto">
-        <b-nav-item href="#">Realtime</b-nav-item>
-        <b-nav-item href="#">History</b-nav-item>
-      </b-navbar-nav>
-    </b-collapse>
     </b-container>
   </b-navbar>
   
@@ -29,6 +20,17 @@ export default {
       //   this.$router.push("/history")
       // }
     }
+
+
+// <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+//     <b-collapse id="nav-collapse" is-nav >
+//       <b-navbar-nav class="ml-auto">
+//         <b-nav-item href="#">Realtime</b-nav-item>
+//         <b-nav-item href="#">History</b-nav-item>
+//       </b-navbar-nav>
+//     </b-collapse>
+
 }
 </script>
 
@@ -39,3 +41,4 @@ a.nav-link{
 }
 
 </style>
+
